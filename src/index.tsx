c40dafeb8574a7699ff8fc51011fc49dc230ecc0
barzin <barzin144@yahoo.com>
2021-06-23 13:43:56 +0430

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./styles/style.scss";
+
+import App from "./components/App";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+    <App />,
+  rootElement
+);
---
+++ webpack.common.js
@@ -0,0 +1,43 @@
+module.exports = {
+  entry: {
+    main: './src/index.tsx'
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.jsx', '.png', '.jpg', '.svg']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: ['html-loader']
+      },
+      {
+        test: /\.(svg|png|jpg|gif)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[hash].[ext]',
+            outputPath: 'imgs'
+          }
+        }
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.tsx$/,
+        exclude: /node_modules/,
+        use: "ts-loader"
+      }
+    ]
+  }
+};
