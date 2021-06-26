10992462bb11dd795701ecc77b47a6d891ffceb8
barzin <barzin144@yahoo.com>
2021-06-26 16:05:12 +0430

    add search

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,17 +1,30 @@
 import React from "react";
 import logoImage from "../images/logo.png";
-import { getGenres } from "../helper";
+import noImage from "../images/noImage.svg";
+import { cancelMovieSearchToken, getGenres, getMoviesByKeyword, thumbnailPath } from "../helper";
 
-interface genre {
+interface Genre {
     id: number,
     name: string
 }
 
+interface SearchResult {
+    id: number,
+    poster_path: string,
+    title: string,
+    vote_average: number,
+    release_date: string
+}
+
 const Header = () => {
+    const node: any = React.useRef();
     const [mobileMenuOpen, setMobileMenuOpen] = React.useState(false);
     const [mobileSearchBoxOpen, setMobileSearchBoxOpen] = React.useState(false);
     const [openDropdown, setopenDropdown] = React.useState(false);
-    const [genres, setGenres] = React.useState([] as genre[]);
+    const [genres, setGenres] = React.useState([] as Genre[]);
+    const [searchResult, setSearchResult] = React.useState(null as SearchResult[]);
+    const [searchResultLoading, setSearchResultLoading] = React.useState(false);
+    const [showSearchResult, setShowSearchResult] = React.useState(false);
 
     React.useEffect(() => {
         getGenres().then((respone) => {
@@ -25,14 +38,47 @@ const Header = () => {
     const showDropdown = () => {
         setopenDropdown(true);
     }
-
     const toggleDropdown = () => {
         setopenDropdown(!openDropdown);
+    }
+    const toggleMobileSearchBox = () => {
+        setMobileSearchBoxOpen(!mobileSearchBoxOpen);
+        setShowSearchResult(false);
+        setSearchResult(null);
     }
     const hamburgerButtonClickHandler = () => {
         setMobileMenuOpen(!mobileMenuOpen);
     }
 
+    const closeSearchResult = () => {
+        cancelMovieSearchToken();
+        setSearchResultLoading(false);
+        setShowSearchResult(false);
+        setSearchResult(null);
+    }
+    const handleOutSideClick = (e: any) => {
+        if (!node.current.contains(e.target)) {
+            closeSearchResult();
+            document.removeEventListener("click", handleOutSideClick, false);
+        }
+    }
+    const searchBoxChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const keyword = e.target.value;
+        if (keyword.trim().length !== 0) {
+            setSearchResultLoading(true);
+            setShowSearchResult(true);
+            getMoviesByKeyword(keyword).then((response) => {
+                const result: SearchResult[] = response.data.results;
+                setSearchResultLoading(false);
+                setShowSearchResult(true);
+                setSearchResult(result.slice(0, 5));
+                document.addEventListener("click", handleOutSideClick, false);
+            });
+        }
+        else {
+            closeSearchResult();
+        }
+    }
     return (
         <header className={`header ${mobileMenuOpen ? "open" : ""}`}>
             <div className={`overlay ${mobileMenuOpen ? "open" : ""}`}></div>
@@ -60,20 +106,51 @@ const Header = () => {
                         <div>
                             <a className="header__links__title" href="#">Movies</a>
                         </div>
-                        <div className="header__searchBox">
-                            <input placeholder="Enter your keywords..." />
+                        <div ref={node} className="header__searchBox">
+                            <input onChange={searchBoxChange} placeholder="Enter your keywords..." />
                             <div className="header__searchBox__searchButton"></div>
+                            {showSearchResult && <div className="header__searchBox__result">
+                                {searchResultLoading && <div className="loading"></div>}
+                                {!searchResultLoading && !!searchResult &&
+                                    <div>
+                                        {searchResult.map((movie) =>
+                                            <div key={movie.id} onClick={() => console.log("click")} className="header__searchBox__result__item flex">
+                                                <img width="60" height="90" src={movie.poster_path === null ? noImage : `${thumbnailPath}/${movie.poster_path}`} />
+                                                <div>
+                                                    <a>{movie.title}</a>
+                                                    <small><i className="far fa-calendar-alt"></i> {movie.release_date}</small>
+                                                    <small><i className="fas fa-star"></i> {movie.vote_average}</small>
+                                                </div>
+                                            </div>
+                                        )}
+                                    </div>}
+                            </div>}
                         </div>
                     </div>
                 </div>
-
-                {mobileSearchBoxOpen && <div className="header__searchBox mobileSearchBox hide-for-desktop flex">
-                    <input placeholder="Enter your keywords..." />
-                    <div className="header__searchBox__searchButton" onClick={() => console.log("search")}></div>
-                    <div onClick={() => setMobileSearchBoxOpen(false)} className="mobileSearchBox-close"></div>
-                </div>}
-                {!mobileSearchBoxOpen && <div onClick={() => setMobileSearchBoxOpen(true)} className="header__mobileSearch hide-for-desktop"></div>}
+                {!mobileSearchBoxOpen && !mobileMenuOpen && <div onClick={toggleMobileSearchBox} className="header__mobileSearchIcon hide-for-desktop"></div>}
             </nav>
+            {mobileSearchBoxOpen && <div className="header__searchBox mobileSearchBox hide-for-desktop flex">
+                <input onChange={searchBoxChange} placeholder="Enter your keywords..." />
+                <div className="header__searchBox__searchButton" onClick={() => console.log("search")}></div>
+                <div onClick={toggleMobileSearchBox} className="mobileSearchBox-close"></div>
+                {showSearchResult && <div className="header__searchBox__result">
+                    {searchResultLoading && <div className="loading"></div>}
+                    {!searchResultLoading && !!searchResult &&
+                        <div>
+                            {searchResult.map((movie) =>
+                                <div key={movie.id} className="header__searchBox__result__item flex">
+                                    <img width="60" height="90" src={movie.poster_path === null ? noImage : `${thumbnailPath}/${movie.poster_path}`} />
+                                    <div>
+                                        <a>{movie.title}</a>
+                                        <small><i className="far fa-calendar-alt"></i> {movie.release_date}</small>
+                                        <small><i className="fas fa-star"></i> {movie.vote_average}</small>
+                                    </div>
+                                </div>
+                            )}
+                        </div>}
+                </div>}
+            </div>}
             {mobileMenuOpen && <div className="header__menu container container--px hide-for-desktop">
                 <div className="divider"></div>
                 <a href="#">Home</a>
--- src/helper.ts
+++ src/helper.ts
@@ -1,10 +1,28 @@
-import axios, { AxiosResponse } from "axios";
+import axios, { AxiosResponse, CancelTokenSource } from "axios";
+
+let cancelTokenSource: CancelTokenSource;
 
 const config = {
     baseURL: 'https://api.themoviedb.org/3',
     headers: { Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0NmRjZGZlMjJiN2M5ODIwMmMwY2FiOGM0YWFhNGE0ZCIsInN1YiI6IjYwZDMwODJlMzVjMzBhMDA3M2Q5YjQ0YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.lXL2hpo8gSurs41a1xLIZGzcoCj9wanresHwGv_apPs` }
 };
 
+export const thumbnailPath = "https://image.tmdb.org/t/p/w154";
+
 export const getGenres = (): Promise<AxiosResponse<any>> => {
     return axios.get('/genre/movie/list', config);
+}
+
+export const getMoviesByKeyword = (keyword: string): Promise<AxiosResponse<any>> => {
+    if (typeof cancelTokenSource != typeof undefined) {
+        cancelTokenSource.cancel("Operation canceled due to new request.");
+    }
+
+    cancelTokenSource = axios.CancelToken.source();
+
+    return axios.get(`/search/movie?query=${keyword}`, { ...config, cancelToken: cancelTokenSource.token });
+}
+
+export const cancelMovieSearchToken = () => {
+    cancelTokenSource.cancel("Operation canceled due to new request.");
 }
