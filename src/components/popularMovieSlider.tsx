cc70fd0dd5d524a19fc6d64be466215e9cc3bd73
barzin <barzin144@yahoo.com>
2021-06-27 13:10:26 +0430

    fixed slider responsive

--- src/components/popularMovieSlider.tsx
+++ src/components/popularMovieSlider.tsx
@@ -24,7 +24,7 @@ const PopularMovieSlider = () => {
     dots: true,
     infinite: true,
     autoplay: true,
-    arrows:false,
+    arrows: false,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1
@@ -35,14 +35,18 @@ const PopularMovieSlider = () => {
       {!!popularMovies && <Slider {...settings}>
         {popularMovies.map((movie) => {
           return <div key={movie.id} className="slider__slide">
-            <div className="slider__slide__image" style={{backgroundImage:`url(${backdrop_path}/${movie.backdrop_path})`}}>
-            <div className="slider__info" >
-              <h1>{movie.title}</h1>
-              <small><i className="far fa-calendar-alt"></i> {movie.release_date}</small>
-              <small><i className="fas fa-star"></i> {movie.vote_average}</small>
-              <p className="hide-for-mobile">{movie.overview}</p>
+            <div className="slider__slide__image" style={{ backgroundImage: `url(${backdrop_path}/${movie.backdrop_path})` }}>
+              <div className="container">
+                <div className="slider__info" >
+                  <h1>{movie.title}</h1>
+                  <div className="slider__info__meta">
+                  <a><i className="far fa-calendar-alt"></i> {movie.release_date}</a>
+                  <a><i className="fas fa-star"></i> {movie.vote_average}</a>
+                  </div>
+                  <div className="slider__info__desc">{movie.overview}</div>
+                </div>
+              </div>
             </div>
-            </div> 
           </div>
         })}
       </Slider>}
