b0a94152922b9ab65fe66f6c3488480a7f5d95c4
Barzin <barzin.assa@elfo.com>
2022-04-27 12:25:32 +0800

    test for heroku

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { getGenres } from "../helper";
-import Casts from "./Casts";
+import Casts from "./casts";
 import Genres from "./genres";
 import Header from "./header";
 import Home from "./home";
@@ -9,41 +9,41 @@ import Movies from "./movies";
 import NotFound from "./notFound";
 
 interface Genre {
-    id: number,
-    name: string
+  id: number;
+  name: string;
 }
 
 const App = () => {
-    const [genres, setGenres] = React.useState([] as Genre[]);
+  const [genres, setGenres] = React.useState([] as Genre[]);
 
-    React.useEffect(() => {
-        getGenres().then((respone) => {
-            setGenres(respone.data.genres.map((genre: any) => ({ id: genre.id, name: genre.name })));
-        });
-    }, []);
+  React.useEffect(() => {
+    getGenres().then((respone) => {
+      setGenres(respone.data.genres.map((genre: any) => ({ id: genre.id, name: genre.name })));
+    });
+  }, []);
 
-    return (
-        <Router>
-            <Header genres={genres}/>
-            <Switch>
-                <Route path="/movies">
-                    <Movies />
-                </Route>
-                <Route path="/genres">
-                    <Genres genres={genres}/>
-                </Route>
-                <Route path="/casts">
-                    <Casts />
-                </Route>
-                <Route exact path="/">
-                    <Home />
-                </Route>
-                <Route path="*">
-                   <NotFound/>
-                </Route>
-            </Switch>
-        </Router>
-    );
-}
+  return (
+    <Router>
+      <Header genres={genres} />
+      <Switch>
+        <Route path="/movies">
+          <Movies />
+        </Route>
+        <Route path="/genres">
+          <Genres genres={genres} />
+        </Route>
+        <Route path="/casts">
+          <Casts />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="*">
+          <NotFound />
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
 
 export default App;
