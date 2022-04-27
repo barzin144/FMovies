4ea3e4367b2aad10e2cb90cc4d0588344566c3eb
Barzin <barzin.assa@elfo.com>
2022-04-27 11:41:49 +0800

    upgrade to webpack 5

--- webpack.common.js
+++ webpack.common.js
@@ -1,46 +1,40 @@
 module.exports = {
   entry: {
-    main: './src/index.tsx'
+    main: "./src/index.tsx",
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js', '.jsx', '.png', '.jpg', '.svg']
+    extensions: [".tsx", ".ts", ".js", ".jsx", ".png", ".jpg", ".svg"],
   },
   module: {
     rules: [
       {
         test: /\.html$/,
-        use: ['html-loader']
+        use: ["html-loader"],
       },
       {
         test: /\.(svg|png|jpg|gif)$/,
-        use: {
-          loader: 'file-loader',
-          options: {
-            name: '[name].[hash].[ext]',
-            outputPath: 'imgs'
-          }
-        }
+        type: "asset/resource",
       },
       {
         test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
         use: [
           {
-            loader: 'file-loader',
+            loader: "file-loader",
             options: {
-              name: '[name].[ext]',
-              outputPath: 'fonts'
-            }
-          }
-        ]
+              name: "[name].[ext]",
+              outputPath: "fonts",
+            },
+          },
+        ],
       },
       {
         test: /\.(tsx$|ts$)/,
         exclude: /node_modules/,
-        use: "ts-loader"
-      }
-    ]
+        use: "ts-loader",
+      },
+    ],
   },
   devServer: {
     historyApiFallback: true,
-  }
+  },
 };
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,29 +1,30 @@
-const path = require('path');
-const common = require('./webpack.common');
-const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const common = require("./webpack.common");
+const { merge } = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = merge(common, {
-    mode: 'development',
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: '/'
-    },
-    plugins: [new HtmlWebpackPlugin({
-        template: './src/index.html'
-    })],
-    module: {
-        rules:
-            [
-                {
-                    test: /\.scss$/,
-                    use: ['style-loader', 'css-loader', 'sass-loader']
-                },
-                {
-                    test: /\.css$/i,
-                    use: ["style-loader", "css-loader"],
-                }
-            ]
-    }
+  mode: "development",
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 });
